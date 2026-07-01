04cd3639607c483fae0d73b1043292875d8bcbdc
haddnist <siddhantgoyal44@gmail.com>
2026-07-01 16:14:43 +0530

    ✨ Keep logo as haddnist, remove name header from homepage hero, keep Siddhant everywhere else

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'haddnist';
+export const SITE_TITLE = 'Siddhant';
 export const SITE_DESCRIPTION = 'Personal Blog';
